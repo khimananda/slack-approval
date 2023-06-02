fce4a025f45a9d07f06c00815a817310288be01d
Khimananda Oli <khimananda.oli@portpro.io>
2023-06-02 10:31:12 +0545

    update url

--- dist/index.js
+++ dist/index.js
@@ -81,7 +81,7 @@ function run() {
                                 },
                                 {
                                     "type": "mrkdwn",
-                                    "text": `[Go to action](${actionsUrl})`
+                                    "text": "<${actionsUrl}|Go to action>"
                                 },
                                 {
                                     "type": "mrkdwn",
